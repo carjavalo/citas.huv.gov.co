16bd354ba781c6f0b6104edfdc99e866121dd0af
Innovación y Desarrollo - HUV <innovacionydesarrollo@correohuv.gov.co>
2026-02-08 13:37:01 -0500

    Fix 419: force HTTPS behind proxy + URL facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\pagination\paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
     {
         paginator::useBootstrap();
         Schema::defaultStringLength(191);
-        //
+
+        // Force HTTPS when behind a proxy (cPanel/LiteSpeed)
+        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+            URL::forceScheme('https');
+        }
     }
 }
